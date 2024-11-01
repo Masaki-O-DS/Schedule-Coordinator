7ec92dcfe5c4c9877bf4f3f2d63b14a5e02d7ff4
Masaki Ogawa <pataporasu@gmail.com>
2024-11-02 08:36:30 +0900

    不要な日程をボタンを押して削除する機能を追加

--- src/Components/Calender.jsx
+++ src/Components/Calender.jsx
@@ -8,6 +8,7 @@ const Calendar = () => {
     endDate: null,
   });
   const [dayList, setDayList] = useState([]);
+  const [dayId, setDayId] = useState("");
 
   const getDatesList = (startDate, endDate) => {
     return eachDayOfInterval({ start: startDate, end: endDate });
@@ -30,6 +31,14 @@ const Calendar = () => {
     setDayList([]);
   };
 
+  const handleDeleteDay = (e) => {
+    console.log(e.target.id);
+    const targetButtonId = e.target.id;
+    const newDayList = dayList.toSpliced(targetButtonId, 1);
+    console.log(newDayList);
+    setDayList(newDayList);
+  };
+
   return (
     <div className="w-full h-full flex-col justify-normal flex items-center">
       <div className="px-10 pt-3 w-3/4 flex  items-center">
@@ -46,21 +55,27 @@ const Calendar = () => {
         />
         <button
           className="w-20 h-8 ml-5 bg-red-400 rounded hover:bg-red-500 hover:text-white duration-200 active:scale-90"
-          onClick={handleResetDate}
+          onClick={(e) => handleResetDate(e)}
         >
           reset
         </button>
       </div>
 
-      <div className=" gap-2 mt-10 pl-14 pr-10 grid-cols-14">
+      <p className="mt-10">
+        不要な日程は下のボタンを押して<span className="text-red-500">削除</span>
+        してください
+      </p>
+      <div className=" gap-2  pl-14 pr-10 grid-cols-14">
         {dayList.length > 0 &&
           dayList.map((dayobj, index) => {
             const month = dayobj.getMonth();
             const day = dayobj.getDate();
             return (
               <button
-                className="bg-white rounded border border-gray-400 w-20 mt-3 px-3 mx-3 text-sm p-1 hover:bg-slate-500 flex-wrap "
+                className="bg-white rounded border border-gray-400 w-20 mt-3 px-3 mx-3 text-sm p-1 hover:bg-red-500 duration-200 active:scale-90 flex-wrap "
+                id={index}
                 key={index}
+                onClick={handleDeleteDay}
               >{`${month}/${day}`}</button>
             );
           })}
